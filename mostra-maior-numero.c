087725bba0a1c3c5854623884262458bfdab49cb
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-04 22:54:35 -0300

    Update mostra-maior-numero.c

--- mostra-maior-numero.c
+++ mostra-maior-numero.c
@@ -1,21 +1,16 @@
-//Faça um programa que receba dois números e mostre o maior.
+/*4-Faça um programa que receba dois números e mostre o maior.*/
 #include <stdio.h>
-
 int main(){
-	int num1, num2;
+  int num1, num2;
 
-	printf("Digite dois numeros separados por espaco: ");
+  printf("Digite dois numeros separados por espaco: ");
+  scanf("%d %d", &num1, &num2);
 
-	scanf("%d %d", &num1, &num2);
-
-	if (num1 > num2){
-		num1 = num1;
-	}
-
-	else{
-		if (num1 < num2){
-			num1 = num2;
-		}
-	}
-	printf("O maior numero e: %d", num1);
+  if (num1 > num2){
+    num1 = num1;
+  }
+  else if (num1 < num2){
+      num1 = num2;
+  }
+  printf("O maior numero e: %d", num1);
 }
