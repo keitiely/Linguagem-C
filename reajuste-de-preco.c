63fca867503f93784a2116ee3661f29303941863
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-04 23:13:56 -0300

    Update reajuste-de-preco.c

--- reajuste-de-preco.c
+++ reajuste-de-preco.c
@@ -1,37 +1,27 @@
-/*Um supermercado deseja reajustar os preços de seus produtos usando o seguinte critério: o produto poderá ter seu preço aumentado ou diminuído.
+/*8-Um supermercado deseja reajustar os preços de seus produtos usando o seguinte critério: o produto poderá ter seu preço aumentado ou diminuído.
 Para alterar o preço o produto deve preencher pelo menos um dos requisitos a seguir:
 Faça um programa que receba o preço atual e a venda mensal média do produto, calcule e mostre o novo preço.*/
 #include <stdio.h>
-
 int main(){
 
-	float vendaM, precoA, reajus;
+  float vendaM, precoA, reajus;
 
-	printf("Digite o Preco atual e a Venda mensal do seu produto separado por espaco: ");
-	scanf("%f %f", &precoA, &vendaM);
+  printf("Digite o Preco atual e a Venda mensal do seu produto separado por espaco: ");
+  scanf("%f %f", &precoA, &vendaM);
 
-	// primeira requisicao
-	if (vendaM < 500 || precoA < 30){
-		reajus = precoA + (precoA * 10 / 100);
-
-		printf("O novo preco e %.2f", reajus);
-	}
-
-	// segunda requisicao
-	else{
-		if (vendaM >= 500 && vendaM < 1200 || precoA >= 30 && precoA < 80){
-			reajus = precoA + (precoA * 15 / 100);
-
-			printf("O novo preco e %.2f", reajus);
-		}
-
-		// terceira requisicao
-		else{
-			if (vendaM >= 1200 || precoA >= 80){
-				reajus = precoA - (precoA * 20 / 100);
-
-				printf("O novo preco e %.2f", reajus);
-			}
-		}
-	}
+  // primeira requisicao
+  if (vendaM < 500 || precoA < 30){
+    reajus = precoA + (precoA * 10 / 100);
+    printf("O novo preco e %.2f", reajus);
+  }
+ // segunda requisicao
+  else if (vendaM >= 500 && vendaM < 1200 || precoA >= 30 && precoA < 80){
+      reajus = precoA + (precoA * 15 / 100);
+    printf("O novo preco e %.2f", reajus);
+  }
+ // terceira requisicao
+  else if (vendaM >= 1200 || precoA >= 80){
+      reajus = precoA - (precoA * 20 / 100);
+     printf("O novo preco e %.2f", reajus);
+  }
 }
