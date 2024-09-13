2d47e6b5cbe8b76040cff15340ed4029a0e9bf3c
Keitiely Silva <162761003+keitiely@users.noreply.github.com>
2024-09-13 20:33:31 -0300

    Update criptografando.c

--- criptografando.c
+++ criptografando.c
@@ -1,4 +1,11 @@
-/*1-Todos os dados são transmitidos como inteiros de quatro dígitos. Seu aplicativo deve ler um inteiro de quatro dígitos inserido pelo usuário e criptografá-lo da seguinte maneira: substitua cada dígito pelo resultado da adição de 7 ao dígito, obtendo o restante depois da divisão do novo valor por 10. Troque então o primeiro dígito pelo terceiro e o segundo dígito pelo quarto. Então, imprima o inteiro criptografado. Escreva um aplicativo separado que receba a entrada de um inteiro de quatro dígitos criptografado e o descriptografe (revertendo o esquema de criptografia) para formar o número original. [Projeto de leitura opcional: pesquise a “criptografia de chave públicaˮ em geral e o esquema de chave pública específica PGP (Pretty Good Privacy). Você também pode querer investigar o esquema RSA, que é amplamente usado em aplicativos robustos industriais.]*/
+/*1-Todos os dados são transmitidos como inteiros de quatro dígitos.
+Seu aplicativo deve ler um inteiro de quatro dígitos inserido pelo usuário e criptografá-lo da seguinte maneira:
+substitua cada dígito pelo resultado da adição de 7 ao dígito, obtendo o restante depois da divisão do novo valor por 10. 
+Troque então o primeiro dígito pelo terceiro e o segundo dígito pelo quarto. Então, imprima o inteiro criptografado. 
+Escreva um aplicativo separado que receba a entrada de um inteiro de quatro dígitos criptografado e o descriptografe 
+(revertendo o esquema de criptografia) para formar o número original. [Projeto de leitura opcional: pesquise a “criptografia de chave públicaˮ
+em geral e o esquema de chave pública específica PGP (Pretty Good Privacy). Você também pode querer investigar o esquema RSA, que é amplamente 
+usado em aplicativos robustos industriais.]*/
 #include <stdio.h>
 int main(){
   int num, aux1, aux2, num1, num2, num3, num4;
